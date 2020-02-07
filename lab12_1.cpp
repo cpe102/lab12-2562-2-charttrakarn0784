e9b76936f9449acf11442e26314f1be2d390ca0a
lab302-15 <lab302-15@labeng.local>
2020-02-07 15:56:19 +0700

    lab12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -7,7 +7,7 @@ void stat(const double[],int,double []);
 int main()
 {
     double A[] = {1.2,-3.5,6.9,7.8,12.5,-0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+    int N = sizeof(A)/sizeof(A[0]); //TODO size of array's element
     double B[4];
     stat(A,N,B);
     cout << "Average = " << B[0];
@@ -17,4 +17,21 @@ int main()
     return 0;
 }
 
-//Write definition of stat() here 
+void stat(const double A[],int N,double B[])
+{
+    double avg=0,max = A[0],min = A[0],sum=0, SD ;
+    for(int i = 0;i<N;i++)
+    {
+        avg += A[i];
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+        sum += pow(A[i],2);
+    }
+    avg /= N;
+    
+    SD = sqrt(sum/N-pow(avg,2));
+    B[0] = avg;
+    B[1] = SD;
+    B[2] = max;
+    B[3] = min;
+}   
